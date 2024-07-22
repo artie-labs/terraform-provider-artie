8a1a1295435f83984688e2bc40e10615c74d2786
Dana Fallon <8871189+danafallon@users.noreply.github.com>
2024-07-22 10:54:13 -0700

    Rename more

--- examples/provider/provider.tf
+++ examples/provider/provider.tf
@@ -1,3 +1,3 @@
-provider "scaffolding" {
+provider "artie" {
   # example configuration here
 }
--- internal/provider/example_resource_test.go
+++ internal/provider/example_resource_test.go
@@ -19,14 +19,14 @@ func TestAccExampleResource(t *testing.T) {
 			{
 				Config: testAccExampleResourceConfig("one"),
 				Check: resource.ComposeAggregateTestCheckFunc(
-					resource.TestCheckResourceAttr("scaffolding_example.test", "configurable_attribute", "one"),
-					resource.TestCheckResourceAttr("scaffolding_example.test", "defaulted", "example value when not configured"),
-					resource.TestCheckResourceAttr("scaffolding_example.test", "id", "example-id"),
+					resource.TestCheckResourceAttr("artie_example.test", "configurable_attribute", "one"),
+					resource.TestCheckResourceAttr("artie_example.test", "defaulted", "example value when not configured"),
+					resource.TestCheckResourceAttr("artie_example.test", "id", "example-id"),
 				),
 			},
 			// ImportState testing
 			{
-				ResourceName:      "scaffolding_example.test",
+				ResourceName:      "artie_example.test",
 				ImportState:       true,
 				ImportStateVerify: true,
 				// This is not normally necessary, but is here because this
@@ -39,7 +39,7 @@ func TestAccExampleResource(t *testing.T) {
 			{
 				Config: testAccExampleResourceConfig("two"),
 				Check: resource.ComposeAggregateTestCheckFunc(
-					resource.TestCheckResourceAttr("scaffolding_example.test", "configurable_attribute", "two"),
+					resource.TestCheckResourceAttr("artie_example.test", "configurable_attribute", "two"),
 				),
 			},
 			// Delete testing automatically occurs in TestCase
@@ -49,7 +49,7 @@ func TestAccExampleResource(t *testing.T) {
 
 func testAccExampleResourceConfig(configurableAttribute string) string {
 	return fmt.Sprintf(`
-resource "scaffolding_example" "test" {
+resource "artie_example" "test" {
   configurable_attribute = %[1]q
 }
 `, configurableAttribute)
