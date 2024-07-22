9a28717c0a6800d000c8886f0f07b6180f87c4b5
Dana Fallon <8871189+danafallon@users.noreply.github.com>
2024-07-22 13:42:13 -0700

    Remove unused

--- internal/provider/provider_test.go
+++ internal/provider/provider_test.go
@@ -15,9 +15,3 @@ import (
 var testAccProtoV6ProviderFactories = map[string]func() (tfprotov6.ProviderServer, error){
 	"artie": providerserver.NewProtocol6WithError(New("test")()),
 }
-
-// func testAccPreCheck(t *testing.T) {
-// 	// You can add code here to run prior to any test case execution, for example assertions
-// 	// about the appropriate environment variables being set are common to see in a pre-check
-// 	// function.
-// }
