2b9945463eb92b14708ea0a2f67ca1353bcf8693
Dana Fallon <8871189+danafallon@users.noreply.github.com>
2024-07-23 14:52:42 -0700

    Docs & formatting

--- examples/deployments/main.tf
+++ examples/deployments/main.tf
@@ -11,12 +11,12 @@ provider "artie" {
 }
 
 import {
-	to = artie_deployment.example
-	id = "38d5d2db-870a-4a38-a76c-9891b0e5122d"
+  to = artie_deployment.example
+  id = "38d5d2db-870a-4a38-a76c-9891b0e5122d"
 }
 
 resource "artie_deployment" "example" {
-	name = "new name"
+  name = "new name"
 }
 
 # data "artie_deployments" "example" {}
