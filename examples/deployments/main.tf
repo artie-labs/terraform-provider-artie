cb7f838599006245d5bf3afe50cb654dc21afacb
Dana Fallon <8871189+danafallon@users.noreply.github.com>
2024-07-23 14:52:00 -0700

    Implement Read for deployment resource

--- examples/deployments/main.tf
+++ examples/deployments/main.tf
@@ -10,8 +10,17 @@ provider "artie" {
   endpoint = "http://0.0.0.0:8000"
 }
 
-data "artie_deployments" "example" {}
-
-output "deployments" {
-  value = data.artie_deployments.example.deployments
+import {
+	to = artie_deployment.example
+	id = "38d5d2db-870a-4a38-a76c-9891b0e5122d"
 }
+
+resource "artie_deployment" "example" {
+	name = "new name"
+}
+
+# data "artie_deployments" "example" {}
+
+# output "deployments" {
+#   value = data.artie_deployments.example.deployments
+# }
