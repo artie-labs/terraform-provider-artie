a9e8b53546843765f1ee89e496de308643cfc889
Dana Fallon <8871189+danafallon@users.noreply.github.com>
2024-07-29 17:36:22 -0700

    Update example config to use postgres & snowflake

--- examples/deployments/main.tf
+++ examples/deployments/main.tf
@@ -11,83 +11,95 @@ provider "artie" {
 }
 
 import {
-  to = artie_destination.bigquery
-  id = "fa7d4efc-3957-41e5-b29c-66e2d49bffde"
-}
-
-variable "mongodb_password" {
-  type      = string
-  sensitive = true
-}
-
-variable "gcp_creds" {
-  type      = string
-  sensitive = true
-}
-
-resource "artie_destination" "bigquery" {
-  name  = "BigQuery"
-  label = "BigQuery"
-  config = {
-    gcp_location         = "us"
-    gcp_project_id       = "artie-labs"
-    gcp_credentials_data = var.gcp_creds
-  }
+  to = artie_destination.snowflake
+  id = "51b180a0-fbb9-49a2-ab45-cb46d913416d"
 }
 
 import {
-  to = artie_deployment.example
-  id = "38d5d2db-870a-4a38-a76c-9891b0e5122d"
+  to = artie_deployment.dev_postgres_to_snowflake
+  id = "c3dfa503-b6ae-48f3-a6b1-8491a506126d"
 }
 
-resource "artie_deployment" "example" {
-  name = "MongoDB ➡️ BigQuery"
+variable "snowflake_password" {
+  type      = string
+  sensitive = true
+}
+variable "postgres_password" {
+  type      = string
+  sensitive = true
+}
+
+resource "artie_destination" "snowflake" {
+  name  = "Snowflake"
+  label = "Snowflake (Partner Account)"
+  config = {
+    snowflake_account_url = "https://znb46775.snowflakecomputing.com"
+    username              = "tang8330"
+    password              = var.snowflake_password
+    snowflake_virtual_dwh = "compute_wh"
+  }
+}
+
+resource "artie_deployment" "dev_postgres_to_snowflake" {
+  name = "Dev PostgreSQL > Snowflake"
   source = {
-    name = "MongoDB"
+    name = "PostgreSQL"
     config = {
-      database = "myFirstDatabase"
-      host     = "mongodb+srv://cluster0.szddg49.mongodb.net/"
-      port     = 0
-      user     = "artie"
-      password = var.mongodb_password
+      host     = "db-postgresql-sfo3-03243-do-user-13261354-0.c.db.ondigitalocean.com"
+      port     = 25060
+      database = "prod_dump_july_2024_4cvzb"
+      user     = "doadmin"
+      password = var.postgres_password
       dynamodb = {}
     }
     tables = [
       {
-        name   = "customers"
-        schema = ""
-        advanced_settings = {
-          skip_delete = false
-        }
-      },
-      {
-        name              = "stock"
-        schema            = ""
+        name              = "invite"
+        schema            = "public"
         advanced_settings = {}
       }
     ]
   }
-  destination_uuid = artie_destination.bigquery.uuid
+  destination_uuid = artie_destination.snowflake.uuid
   destination_config = {
-    dataset = "customers"
+    database = "DEV_TEST"
+    schema   = "PUBLIC"
   }
-  advanced_settings = {
-    enable_soft_delete     = true
-    flush_interval_seconds = 60
-  }
+  advanced_settings = {}
 }
 
-# resource "artie_destination" "bigquery2" {
+# import {
+#   to = artie_destination.bigquery
+#   id = "fa7d4efc-3957-41e5-b29c-66e2d49bffde"
+# }
+
+# variable "mongodb_password" {
+#   type      = string
+#   sensitive = true
+# }
+
+# variable "gcp_creds" {
+#   type      = string
+#   sensitive = true
+# }
+
+# resource "artie_destination" "bigquery" {
 #   name  = "BigQuery"
-#   label = "BigQuery2 (to delete)"
+#   label = "BigQuery"
 #   config = {
-#     gcp_location   = "us"
-#     gcp_project_id = "artie-labs"
+#     gcp_location         = "us"
+#     gcp_project_id       = "artie-labs"
+#     gcp_credentials_data = var.gcp_creds
 #   }
 # }
 
-# resource "artie_deployment" "newdeployment" {
-#   name = "New deployment from tf"
+# import {
+#   to = artie_deployment.example
+#   id = "38d5d2db-870a-4a38-a76c-9891b0e5122d"
+# }
+
+# resource "artie_deployment" "example" {
+#   name = "MongoDB ➡️ BigQuery"
 #   source = {
 #     name = "MongoDB"
 #     config = {
@@ -95,6 +107,7 @@ resource "artie_deployment" "example" {
 #       host     = "mongodb+srv://cluster0.szddg49.mongodb.net/"
 #       port     = 0
 #       user     = "artie"
+#       password = var.mongodb_password
 #       dynamodb = {}
 #     }
 #     tables = [
@@ -105,11 +118,19 @@ resource "artie_deployment" "example" {
 #           skip_delete = false
 #         }
 #       },
+#       {
+#         name              = "stock"
+#         schema            = ""
+#         advanced_settings = {}
+#       }
 #     ]
 #   }
-#   destination_uuid = artie_destination.bigquery2.uuid
+#   destination_uuid = artie_destination.bigquery.uuid
 #   destination_config = {
 #     dataset = "customers"
 #   }
-#   advanced_settings = {}
+#   advanced_settings = {
+#     enable_soft_delete     = true
+#     flush_interval_seconds = 60
+#   }
 # }
