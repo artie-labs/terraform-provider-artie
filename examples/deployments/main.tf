d1d82e0b25eb8ef91e278cefb24312c7209d57ae
Dana Fallon <8871189+danafallon@users.noreply.github.com>
2024-08-19 15:59:38 -0700

    Organize more

--- examples/deployments/main.tf
+++ examples/deployments/main.tf
@@ -1,20 +1,3 @@
-terraform {
-  required_providers {
-    artie = {
-      source = "artie.com/terraform/artie"
-    }
-  }
-}
-
-variable "artie_api_key" {
-  type      = string
-  sensitive = true
-}
-
-provider "artie" {
-  api_key = var.artie_api_key
-}
-
 variable "snowflake_password" {
   type      = string
   sensitive = true
--- examples/provider/provider.tf
+++ examples/provider/provider.tf
@@ -1,3 +1,11 @@
+terraform {
+  required_providers {
+    artie = {
+      source = "artie.com/terraform/artie"
+    }
+  }
+}
+
 variable "artie_api_key" {
   type      = string
   sensitive = true
