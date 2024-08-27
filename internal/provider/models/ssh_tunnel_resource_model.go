ab5443cce9fd6208443a40a1d0140ba9bb5a61a2
Dana Fallon <8871189+danafallon@users.noreply.github.com>
2024-08-26 17:13:59 -0700

    Move SSHTunnelResourceModel methods into the same file as the struct

--- internal/provider/models/ssh_tunnel_resource_model.go
+++ internal/provider/models/ssh_tunnel_resource_model.go
@@ -1,6 +1,10 @@
 package models
 
-import "github.com/hashicorp/terraform-plugin-framework/types"
+import (
+	"github.com/hashicorp/terraform-plugin-framework/types"
+
+	"terraform-provider-artie/internal/artieclient"
+)
 
 type SSHTunnelResourceModel struct {
 	UUID      types.String `tfsdk:"uuid"`
@@ -9,4 +13,30 @@ type SSHTunnelResourceModel struct {
 	Port      types.Int32  `tfsdk:"port"`
 	Username  types.String `tfsdk:"username"`
 	PublicKey types.String `tfsdk:"public_key"`
+}
+
+func (s *SSHTunnelResourceModel) UpdateFromAPIModel(apiModel artieclient.SSHTunnel) {
+	s.UUID = types.StringValue(apiModel.UUID.String())
+	s.Name = types.StringValue(apiModel.Name)
+	s.Host = types.StringValue(apiModel.Host)
+	s.Port = types.Int32Value(apiModel.Port)
+	s.Username = types.StringValue(apiModel.Username)
+	s.PublicKey = types.StringValue(apiModel.PublicKey)
+}
+
+func (s SSHTunnelResourceModel) ToAPIBaseModel() artieclient.BaseSSHTunnel {
+	return artieclient.BaseSSHTunnel{
+		Name:      s.Name.ValueString(),
+		Host:      s.Host.ValueString(),
+		Port:      s.Port.ValueInt32(),
+		Username:  s.Username.ValueString(),
+		PublicKey: s.PublicKey.ValueString(),
+	}
+}
+
+func (s SSHTunnelResourceModel) ToAPIModel() artieclient.SSHTunnel {
+	return artieclient.SSHTunnel{
+		UUID:          parseUUID(s.UUID),
+		BaseSSHTunnel: s.ToAPIBaseModel(),
+	}
 }
