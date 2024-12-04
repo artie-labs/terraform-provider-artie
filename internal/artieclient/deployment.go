83c1865d833427a79edaf10f23743a042b25698a
Nathan <148575555+nathan-artie@users.noreply.github.com>
2024-12-04 12:24:17 -0800

    Change "deploy" to "deployment"

--- internal/artieclient/deployment.go
+++ internal/artieclient/deployment.go
@@ -114,7 +114,7 @@ func (DeploymentClient) basePath() string {
 }
 
 type deploymentAPIResponse struct {
-	Deployment deploymentWithAdvSettings `json:"deploy"`
+	Deployment deploymentWithAdvSettings `json:"deployment"`
 }
 
 type validationResponse struct {
@@ -148,7 +148,7 @@ func (dc DeploymentClient) Update(ctx context.Context, deployment Deployment) (D
 	}
 
 	body := map[string]any{
-		"deploy":      deployment,
+		"deployment":  deployment,
 		"startDeploy": true,
 	}
 
