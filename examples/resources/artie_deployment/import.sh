318259b1ab59d85685c7bc9775c365c8988ad935
Robin Tang <rtang.cs@gmail.com>
2025-02-17 15:34:30 -0800

    Update Artie docs to include an example of an import (#112)

---
+++ examples/resources/artie_deployment/import.sh
@@ -0,0 +1,4 @@
+# Import an Artie deployment by using the uuid.
+# https://app.artie.com/deployments/123e4567-e89b-12d3-a456-426614174000/overview
+
+terraform import artie_deployment.default 123e4567-e89b-12d3-a456-426614174000
