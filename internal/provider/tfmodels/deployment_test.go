6f86771d503e8238ea81fdbe18490618e28d5cc5
Robin Tang <rtang.cs@gmail.com>
2025-03-31 14:28:47 -0700

    Adding tests.

--- internal/provider/tfmodels/deployment_test.go
+++ internal/provider/tfmodels/deployment_test.go
@@ -10,27 +10,42 @@ import (
 )
 
 func TestDeploymentFlushConfigFromAPIModel(t *testing.T) {
-	flushObject, diags := types.ObjectValue(flushAttrTypes, map[string]attr.Value{
-		"flush_interval_seconds": types.Int64Value(100),
-		"buffer_rows":            types.Int64Value(5000),
-		"flush_size_kb":          types.Int64Value(1000),
-	})
+	{
+		// nil object
+		var object types.Object
+		var flushConfig *DeploymentFlushConfig
+		diags := object.As(t.Context(), &flushConfig, basetypes.ObjectAsOptions{
+			UnhandledNullAsEmpty:    true,
+			UnhandledUnknownAsEmpty: true,
+		})
 
-	assert.False(t, diags.HasError(), "failed to create flush config")
+		assert.False(t, diags.HasError(), "expected no error when creating nil object")
+		assert.Nil(t, flushConfig)
+	}
+	{
+		// object is set
+		flushObject, diags := types.ObjectValue(flushAttrTypes, map[string]attr.Value{
+			"flush_interval_seconds": types.Int64Value(100),
+			"buffer_rows":            types.Int64Value(5000),
+			"flush_size_kb":          types.Int64Value(1000),
+		})
 
-	var flushConfig *DeploymentFlushConfig
-	flushConfigDiags := flushObject.As(t.Context(), &flushConfig, basetypes.ObjectAsOptions{
-		UnhandledNullAsEmpty:    true,
-		UnhandledUnknownAsEmpty: true,
-	})
+		assert.False(t, diags.HasError(), "failed to create flush config")
 
-	assert.False(t, flushConfigDiags.HasError(), "failed to convert flush config")
-	assert.Equal(t, flushConfig.FlushIntervalSeconds.ValueInt64(), int64(100))
-	assert.Equal(t, flushConfig.BufferRows.ValueInt64(), int64(5000))
-	assert.Equal(t, flushConfig.FlushSizeKB.ValueInt64(), int64(1000))
+		var flushConfig *DeploymentFlushConfig
+		flushConfigDiags := flushObject.As(t.Context(), &flushConfig, basetypes.ObjectAsOptions{
+			UnhandledNullAsEmpty:    true,
+			UnhandledUnknownAsEmpty: true,
+		})
 
-	apiFlushConfig := flushConfig.ToAPIModel()
-	assert.Equal(t, apiFlushConfig.FlushIntervalSeconds, int64(100))
-	assert.Equal(t, apiFlushConfig.BufferRows, int64(5000))
-	assert.Equal(t, apiFlushConfig.FlushSizeKB, int64(1000))
+		assert.False(t, flushConfigDiags.HasError(), "failed to convert flush config")
+		assert.Equal(t, flushConfig.FlushIntervalSeconds.ValueInt64(), int64(100))
+		assert.Equal(t, flushConfig.BufferRows.ValueInt64(), int64(5000))
+		assert.Equal(t, flushConfig.FlushSizeKB.ValueInt64(), int64(1000))
+
+		apiFlushConfig := flushConfig.ToAPIModel()
+		assert.Equal(t, apiFlushConfig.FlushIntervalSeconds, int64(100))
+		assert.Equal(t, apiFlushConfig.BufferRows, int64(5000))
+		assert.Equal(t, apiFlushConfig.FlushSizeKB, int64(1000))
+	}
 }
