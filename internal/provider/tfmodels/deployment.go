f0cf8dbb1cb9849313aa9bb651ad03aafa6c0f7c
Robin Tang <rtang.cs@gmail.com>
2025-03-31 15:45:24 -0700

    Flush Config to use `types.Object` (#149)

--- internal/provider/tfmodels/deployment.go
+++ internal/provider/tfmodels/deployment.go
@@ -3,8 +3,10 @@ package tfmodels
 import (
 	"context"
 
+	"github.com/hashicorp/terraform-plugin-framework/attr"
 	"github.com/hashicorp/terraform-plugin-framework/diag"
 	"github.com/hashicorp/terraform-plugin-framework/types"
+	"github.com/hashicorp/terraform-plugin-framework/types/basetypes"
 
 	"terraform-provider-artie/internal/artieclient"
 )
@@ -21,14 +23,40 @@ type Deployment struct {
 	DataPlaneName            types.String                 `tfsdk:"data_plane_name"`
 
 	// Advanced settings
-	FlushConfig                    *DeploymentFlushConfig `tfsdk:"flush_config"`
-	DropDeletedColumns             types.Bool             `tfsdk:"drop_deleted_columns"`
-	SoftDeleteRows                 types.Bool             `tfsdk:"soft_delete_rows"`
-	IncludeArtieUpdatedAtColumn    types.Bool             `tfsdk:"include_artie_updated_at_column"`
-	IncludeDatabaseUpdatedAtColumn types.Bool             `tfsdk:"include_database_updated_at_column"`
-	OneTopicPerSchema              types.Bool             `tfsdk:"one_topic_per_schema"`
-	PublicationNameOverride        types.String           `tfsdk:"postgres_publication_name_override"`
-	ReplicationSlotOverride        types.String           `tfsdk:"postgres_replication_slot_override"`
+	FlushConfig                    types.Object `tfsdk:"flush_config"`
+	DropDeletedColumns             types.Bool   `tfsdk:"drop_deleted_columns"`
+	SoftDeleteRows                 types.Bool   `tfsdk:"soft_delete_rows"`
+	IncludeArtieUpdatedAtColumn    types.Bool   `tfsdk:"include_artie_updated_at_column"`
+	IncludeDatabaseUpdatedAtColumn types.Bool   `tfsdk:"include_database_updated_at_column"`
+	OneTopicPerSchema              types.Bool   `tfsdk:"one_topic_per_schema"`
+	PublicationNameOverride        types.String `tfsdk:"postgres_publication_name_override"`
+	ReplicationSlotOverride        types.String `tfsdk:"postgres_replication_slot_override"`
+}
+
+type DeploymentFlushConfig struct {
+	FlushIntervalSeconds types.Int64 `tfsdk:"flush_interval_seconds"`
+	BufferRows           types.Int64 `tfsdk:"buffer_rows"`
+	FlushSizeKB          types.Int64 `tfsdk:"flush_size_kb"`
+}
+
+var flushAttrTypes = map[string]attr.Type{
+	"flush_interval_seconds": types.Int64Type,
+	"buffer_rows":            types.Int64Type,
+	"flush_size_kb":          types.Int64Type,
+}
+
+func buildFlushConfig(ctx context.Context, d types.Object) (*DeploymentFlushConfig, diag.Diagnostics) {
+	var flushConfig *DeploymentFlushConfig
+	flushConfigDiags := d.As(ctx, &flushConfig, basetypes.ObjectAsOptions{
+		UnhandledNullAsEmpty:    true,
+		UnhandledUnknownAsEmpty: true,
+	})
+
+	if flushConfigDiags.HasError() {
+		return nil, flushConfigDiags
+	}
+
+	return flushConfig, nil
 }
 
 func (d Deployment) ToAPIBaseModel(ctx context.Context) (artieclient.BaseDeployment, diag.Diagnostics) {
@@ -55,6 +83,12 @@ func (d Deployment) ToAPIBaseModel(ctx context.Context) (artieclient.BaseDeploym
 		return artieclient.BaseDeployment{}, diags
 	}
 
+	flushConfig, flushConfigDiags := buildFlushConfig(ctx, d.FlushConfig)
+	diags.Append(flushConfigDiags...)
+	if diags.HasError() {
+		return artieclient.BaseDeployment{}, diags
+	}
+
 	return artieclient.BaseDeployment{
 		Name:                     d.Name.ValueString(),
 		Source:                   apiSource,
@@ -64,7 +98,7 @@ func (d Deployment) ToAPIBaseModel(ctx context.Context) (artieclient.BaseDeploym
 		SnowflakeEcoScheduleUUID: snowflakeEcoScheduleUUID,
 		DataPlaneName:            d.DataPlaneName.ValueString(),
 		// Advanced settings:
-		FlushConfig:                    d.FlushConfig.ToAPIModel(),
+		FlushConfig:                    flushConfig.ToAPIModel(),
 		DropDeletedColumns:             d.DropDeletedColumns.ValueBoolPointer(),
 		EnableSoftDelete:               d.SoftDeleteRows.ValueBoolPointer(),
 		IncludeArtieUpdatedAtColumn:    d.IncludeArtieUpdatedAtColumn.ValueBoolPointer(),
@@ -102,9 +136,14 @@ func DeploymentFromAPIModel(ctx context.Context, apiModel artieclient.Deployment
 
 	destinationConfig := DeploymentDestinationConfigFromAPIModel(apiModel.DestinationConfig)
 
-	var flushConfig *DeploymentFlushConfig
+	var flushConfig types.Object
 	if apiModel.FlushConfig != nil {
-		flushConfig = DeploymentFlushConfigFromAPIModel(*apiModel.FlushConfig)
+		flushConfigObj, flushConfigDiags := DeploymentFlushConfigFromAPIModel(ctx, *apiModel.FlushConfig)
+		diags.Append(flushConfigDiags...)
+		if diags.HasError() {
+			return Deployment{}, diags
+		}
+		flushConfig = flushConfigObj
 	}
 
 	return Deployment{
@@ -140,13 +179,12 @@ type DeploymentDestinationConfig struct {
 	Folder                types.String `tfsdk:"folder"`
 }
 
-type DeploymentFlushConfig struct {
-	FlushIntervalSeconds types.Int64 `tfsdk:"flush_interval_seconds"`
-	BufferRows           types.Int64 `tfsdk:"buffer_rows"`
-	FlushSizeKB          types.Int64 `tfsdk:"flush_size_kb"`
-}
+func (d *DeploymentFlushConfig) ToAPIModel() *artieclient.FlushConfig {
+	if d == nil {
+		// Support unknown.
+		return nil
+	}
 
-func (d DeploymentFlushConfig) ToAPIModel() *artieclient.FlushConfig {
 	return &artieclient.FlushConfig{
 		FlushIntervalSeconds: d.FlushIntervalSeconds.ValueInt64(),
 		BufferRows:           d.BufferRows.ValueInt64(),
@@ -178,10 +216,10 @@ func DeploymentDestinationConfigFromAPIModel(apiModel artieclient.DestinationCon
 	}
 }
 
-func DeploymentFlushConfigFromAPIModel(apiModel artieclient.FlushConfig) *DeploymentFlushConfig {
-	return &DeploymentFlushConfig{
+func DeploymentFlushConfigFromAPIModel(ctx context.Context, apiModel artieclient.FlushConfig) (types.Object, diag.Diagnostics) {
+	return types.ObjectValueFrom(ctx, flushAttrTypes, DeploymentFlushConfig{
 		FlushIntervalSeconds: types.Int64Value(apiModel.FlushIntervalSeconds),
 		BufferRows:           types.Int64Value(apiModel.BufferRows),
 		FlushSizeKB:          types.Int64Value(apiModel.FlushSizeKB),
-	}
+	})
 }
---
+++ internal/provider/tfmodels/deployment_test.go
@@ -0,0 +1,56 @@
+package tfmodels
+
+import (
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-framework/attr"
+	"github.com/hashicorp/terraform-plugin-framework/types"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestDeploymentFlushConfigFromAPIModel(t *testing.T) {
+	{
+		// zero object
+		var object types.Object
+		flushConfig, diags := buildFlushConfig(t.Context(), object)
+		assert.False(t, diags.HasError(), "expected no error when creating nil object")
+		assert.Nil(t, flushConfig)
+	}
+	{
+		// null object
+		nullObject := types.ObjectNull(flushAttrTypes)
+		flushConfig, diags := buildFlushConfig(t.Context(), nullObject)
+		assert.False(t, diags.HasError(), "expected no error when creating null object")
+		assert.Nil(t, flushConfig)
+	}
+	{
+		// unknown object
+		unknownObject := types.ObjectUnknown(flushAttrTypes)
+		flushConfig, diags := buildFlushConfig(t.Context(), unknownObject)
+		assert.False(t, diags.HasError(), "expected no error when creating unknown object")
+		assert.Nil(t, flushConfig)
+	}
+	{
+		// object is set
+		flushObject, diags := types.ObjectValue(flushAttrTypes, map[string]attr.Value{
+			"flush_interval_seconds": types.Int64Value(100),
+			"buffer_rows":            types.Int64Value(5000),
+			"flush_size_kb":          types.Int64Value(1000),
+		})
+
+		assert.False(t, diags.HasError(), "failed to create flush object")
+
+		flushConfig, diags := buildFlushConfig(t.Context(), flushObject)
+		assert.False(t, diags.HasError(), "failed to create flush config")
+		assert.NotNil(t, flushConfig)
+
+		assert.Equal(t, flushConfig.FlushIntervalSeconds.ValueInt64(), int64(100))
+		assert.Equal(t, flushConfig.BufferRows.ValueInt64(), int64(5000))
+		assert.Equal(t, flushConfig.FlushSizeKB.ValueInt64(), int64(1000))
+
+		apiFlushConfig := flushConfig.ToAPIModel()
+		assert.Equal(t, apiFlushConfig.FlushIntervalSeconds, int64(100))
+		assert.Equal(t, apiFlushConfig.BufferRows, int64(5000))
+		assert.Equal(t, apiFlushConfig.FlushSizeKB, int64(1000))
+	}
+}
