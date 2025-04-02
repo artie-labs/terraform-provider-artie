9745af02d9cc22d47a1bdbef5efcebbfa673475a
Dana Fallon <8871189+danafallon@users.noreply.github.com>
2025-04-01 18:32:52 -0700

    Update example

--- examples/resources/artie_source_reader/resource.tf
+++ examples/resources/artie_source_reader/resource.tf
@@ -1,5 +1,5 @@
 resource "artie_source_reader" "postgres_dev_reader" {
-  name                               = "Postgres Dev Reader"
+  name                               = "Postgres Dev Customers Reader"
   connector_uuid                     = artie_connector.postgres_dev.uuid
   database_name                      = "customers"
   postgres_replication_slot_override = "artie_reader"
