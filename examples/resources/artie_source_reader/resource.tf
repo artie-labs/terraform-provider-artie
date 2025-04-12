3c37806ce45542485a9716644d55222ab9a3e0e8
Dana Fallon <8871189+danafallon@users.noreply.github.com>
2025-04-11 18:39:10 -0700

    Add an example of a shared source reader to the docs (#165)

--- examples/resources/artie_source_reader/resource.tf
+++ examples/resources/artie_source_reader/resource.tf
@@ -1,6 +1,28 @@
+# A source reader that can only be used by one pipeline:
 resource "artie_source_reader" "postgres_dev_reader" {
   name                               = "Postgres Dev Customers Reader"
   connector_uuid                     = artie_connector.postgres_dev.uuid
   database_name                      = "customers"
   postgres_replication_slot_override = "artie_reader"
+  is_shared                          = false
+}
+
+# A source reader that can be used by multiple pipelines (must specify tables):
+resource "artie_source_reader" "postgres_dev_reader" {
+  name                               = "Postgres Dev Customers Reader"
+  connector_uuid                     = artie_connector.postgres_dev.uuid
+  database_name                      = "customers"
+  postgres_replication_slot_override = "artie_reader"
+  is_shared                          = true
+  tables = {
+    "public.account" = {
+      name               = "account"
+      schema             = "public"
+      columns_to_exclude = ["email"]
+    },
+    "public.company" = {
+      name   = "company"
+      schema = "public"
+    }
+  }
 }
