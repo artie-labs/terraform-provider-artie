76239cbdbb3390b5947142d09d93632bd41a88b8
Dana Fallon <8871189+danafallon@users.noreply.github.com>
2025-06-03 14:00:24 -0700

    Remove deprecated resources

--- examples/resources/artie_deployment/import.sh
+++
@@ -1,4 +0,0 @@
-# Import an Artie deployment by using the uuid.
-# https://app.artie.com/deployments/123e4567-e89b-12d3-a456-426614174000/overview
-
-terraform import artie_deployment.default 123e4567-e89b-12d3-a456-426614174000
