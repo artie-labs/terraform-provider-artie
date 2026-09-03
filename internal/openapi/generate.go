9d8c9cabc29f4b6cdea880690369d47d18f0da95
Hermes <hermes@artie.com>
2026-09-03 04:59:00 +0000

    feat: support PostgreSQL failover replication slots

--- internal/openapi/generate.go
+++ internal/openapi/generate.go
@@ -1,3 +1,3 @@
 package openapi
 
-//go:generate go tool oapi-codegen -config ../../oapi-codegen-config.yaml https://raw.githubusercontent.com/artie-labs/artie-api-spec/refs/tags/v1.0.88/openapi.yaml
+//go:generate go tool oapi-codegen -config ../../oapi-codegen-config.yaml https://raw.githubusercontent.com/artie-labs/artie-api-spec/77a9dafb830b6adf379f3028ebd8883016e0a396/openapi.yaml
